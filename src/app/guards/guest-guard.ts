cbda33c792db4075bb124a18fe6ea145a0289077
Andrei Carp <andrei.krp@gmail.com>
2026-07-30 02:09:33 +0300

    [Homework 7] feat: Logout user and redirect to login on 401/403 responses in interceptor
    Signed-off-by: Andrei Carp <andrei.krp@gmail.com>

--- src/app/guards/guest-guard.ts
+++ src/app/guards/guest-guard.ts
@@ -12,4 +12,4 @@ export const guestGuard: CanActivateFn = () => {
   }
 
   return true;
-}
+};
--- src/app/interceptors/authentication-interceptor.ts
+++ src/app/interceptors/authentication-interceptor.ts
@@ -1,11 +1,16 @@
-import { HttpInterceptor, HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptor, HttpInterceptorFn } from '@angular/common/http';
 import LocalStorageUtils from '../utils/local-storage-utils';
+import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
+  const userService = inject(UserService);
+
   if (req.url.includes('login') || req.url.includes('register')) {
     return next(req);
   }
-``
+
   const token: string | null = LocalStorageUtils.getItem(LocalStorageUtils.tokenKey);
   let processedRequest;
   if (token) {
@@ -16,5 +21,12 @@ export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
     processedRequest = req;
   }
 
-  return next(processedRequest);
-}
+  return next(processedRequest).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        userService.logout();
+      }
+      return throwError(() => error);
+    })
+  );
+};
